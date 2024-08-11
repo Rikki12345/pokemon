bb5837d4e7a39465410beee45d02d499fad43d4a
rikki abramson <rikkiabramson@gmail.com>
2024-08-11 12:47:16 -0400

    added pictures

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    return <p>Connected to app.js</p>
+    return <p className= 'card'>Connected to App.js</p>
 }
 export default App;
